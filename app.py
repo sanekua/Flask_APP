15c96700cec8b4e8d79fea5c66cce8f99c42eab5
sanekua <sasha-kuzmyk@mail.ru>
2022-01-30 21:17:03 +0200

    Added run

--- app.py
+++ app.py
@@ -34,9 +34,4 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
